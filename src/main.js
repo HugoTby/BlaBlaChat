a5d25dc7179ec02bd39708d82f2fa8ebb94ce53e
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-03-19 00:27:25 +0100

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -34,14 +34,6 @@ $(".channels-header")[0].addEventListener("click", e => {
 });
 
 
-
-
-
-
-
-
-
-
 function toggleDiv() {
 	var div = document.getElementById('warning-content');
 	if (div.classList.contains('warning-active')) {
@@ -52,3 +44,29 @@ function toggleDiv() {
 		div.classList.add('warning-active');
 	}
 }
+
+/*let input = document.querySelector('input'); // get the input element
+input.addEventListener('input', resizeInput); // bind the "resizeInput" callback on "input" event
+resizeInput.call(input); // immediately call the function*/
+
+document.querySelectorAll('input').forEach(input => {
+	input.addEventListener('input', resizeInput);
+	resizeInput.call(input);
+  })
+  
+  function resizeInput(input) {
+	//console.log(this.value)
+	//if (this.id == "username") {
+	  //this.style.minWidth = "63px"
+	  //this.style.maxWidth = "100px"
+	//} else {
+	  //this.style.minWidth = "100px"
+	//}
+	//this.style.width = (this.value.length + 0.4) + "ch";
+	if (this.value.length <= 3) {
+	  this.style.width = "3.5ch"; // test
+	} else {
+	  this.style.width = (this.value.length + 0.4) + "ch";
+	}
+  }
+	//this.style.minwidth = (this.value.length + 1) + "ch";
