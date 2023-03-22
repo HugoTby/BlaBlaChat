46fe550f11af4dfd6ea2ba9e3c40c74dde63d44f
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-03-22 22:44:41 +0100

    Add files via upload

--- src/sign-up/index.php
+++ src/sign-up/index.php
@@ -1,87 +1,172 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="ltr">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <script type="text/javascript" src="main.js"></script>
+    <meta charset="utf-8">
     <title>BlaBlaChat</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </head>
+
 <body>
-<div>
-    <h1>Create a Multi-Step Form Using HTML, CSS, and JavaScript</h1>
-    <div id="multi-step-form-container">
-        <!-- Form Steps / Progress Bar -->
-        <ul class="form-stepper form-stepper-horizontal text-center mx-auto pl-0">
-            <!-- Step 1 -->
-            <li class="form-stepper-active text-center form-stepper-list" step="1">
-                <a class="mx-2">
-                    <span class="form-stepper-circle">
-                        <span>1</span>
-                    </span>
-                    <div class="label">Account Basic Details</div>
-                </a>
-            </li>
-            <!-- Step 2 -->
-            <li class="form-stepper-unfinished text-center form-stepper-list" step="2">
-                <a class="mx-2">
-                    <span class="form-stepper-circle text-muted">
-                        <span>2</span>
-                    </span>
-                    <div class="label text-muted">Social Profiles</div>
-                </a>
-            </li>
-            <!-- Step 3 -->
-            <li class="form-stepper-unfinished text-center form-stepper-list" step="3">
-                <a class="mx-2">
-                    <span class="form-stepper-circle text-muted">
-                        <span>3</span>
-                    </span>
-                    <div class="label text-muted">Personal Details</div>
-                </a>
-            </li>
+    <div class="area">
+        <ul class="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
         </ul>
-        <!-- Step Wise Form Content -->
-        <form id="userAccountSetupForm" name="userAccountSetupForm" enctype="multipart/form-data" method="POST">
-            <!-- Step 1 Content -->
-            <section id="step-1" class="form-step">
-                <h2 class="font-normal">Account Basic Details</h2>
-                <!-- Step 1 input fields -->
-                <div class="mt-3">
-                    Step 1 input fields goes here..
-                    <input type="text"></input>
+        <div class="container">
+            <header>BlaBlaChat - S'inscrire</header>
+            <div class="progress-bar">
+                <div class="step">
+                    <p>Identité</p>
+                    <div class="bullet">
+                        <span>1</span>
+                    </div>
+                    <div class="check fas fa-check"></div>
                 </div>
-                <div class="mt-3">
-                    <button class="button btn-navigate-form-step" type="button" step_number="2">Next</button>
+                <div class="step">
+                    <p>Classe</p>
+                    <div class="bullet">
+                        <span>2</span>
+                    </div>
+                    <div class="check fas fa-check"></div>
                 </div>
-            </section>
-            <!-- Step 2 Content, default hidden on page load. -->
-            <section id="step-2" class="form-step d-none">
-                <h2 class="font-normal">Social Profiles</h2>
-                <!-- Step 2 input fields -->
-                <div class="mt-3">
-                    Step 2 input fields goes here..
+                <div class="step">
+                    <p>Compte</p>
+                    <div class="bullet">
+                        <span>3</span>
+                    </div>
+                    <div class="check fas fa-check"></div>
                 </div>
-                <div class="mt-3">
-                    <button class="button btn-navigate-form-step" type="button" step_number="1">Prev</button>
-                    <button class="button btn-navigate-form-step" type="button" step_number="3">Next</button>
+                <div class="step">
+                    <p>Aperçu</p>
+                    <div class="bullet">
+                        <span>4</span>
+                    </div>
+                    <div class="check fas fa-check"></div>
                 </div>
-            </section>
-            <!-- Step 3 Content, default hidden on page load. -->
-            <section id="step-3" class="form-step d-none">
-                <h2 class="font-normal">Personal Details</h2>
-                <!-- Step 3 input fields -->
-                <div class="mt-3">
-                    Step 3 input fields goes here..
-                </div>
-                <div class="mt-3">
-                    <button class="button btn-navigate-form-step" type="button" step_number="2">Prev</button>
-                    <button class="button submit-btn" type="submit">Save</button>
-                </div>
-            </section>
-        </form>
+            </div>
+            <div class="form-outer">
+                <form method="post">
+                    <div class="page slide-page">
+                        <div class="title">Informations générales:</div>
+                        <div class="field">
+                            <div class="label">Adresse e-mail<span style="color: red;">*</span></div>
+                            <input type="text">
+                        </div>
+                        <div class="field">
+                            <div class="label">Prénom<span style="color: red;">*</span></div>
+                            <input type="text">
+                        </div>
+                        <div class="field">
+                            <div class="label">Nom<span style="color: red;">*</span></div>
+                            <input type="text">
+                        </div>
+                        <div class="field">
+                            <button class="firstNext next">Suivant</button>
+                        </div>
+                        <h1 style="font-size: 15px;color:red">* Champs obligatoires</h1>
+                    </div>
+
+                    <div class="page">
+                        <div class="title">Information étudiant:</div>
+                        <div class="field">
+                            <div class="label">Classe<span style="color: red;">*</span></div>
+                            <select>
+                                <option>BTS SN 1 - Systèmes Numérique</option>
+                                <option>BTS SN 2 - Systèmes Numérique</option>
+                                <option>BTS CIEL 1 - CyberSécurité Électronique</option>
+                                <option>BTS CIEL 2 - CyberSécurité Électronique</option>
+                                <option>BTS E 1 - Électrotechnique</option>
+                                <option>BTS E 2 - Électrotechnique</option>
+                                <option>BTS MS 1 - Systèmes Energétiques & Fluidiques</option>
+                                <option>BTS MS 2 - Systèmes Energétiques & Fluidiques</option>
+                                <option>BTS FED 1 - Génie Climatique & Fluidique</option>
+                                <option>BTS FED 2 - Génie Climatique & Fluidique</option>
+                                <option>L3 EDD - Energies et Développement Durable</option>
+                            </select>
+                        </div>
+                        <div class="field btns">
+                            <button class="prev-1 prev">Précédent</button>
+                            <button class="next-1 next">Suivant</button>
+                        </div>
+                        <h1 style="font-size: 15px;color:red">* Champs obligatoires</h1>
+                    </div>
+                    <div class="page">
+                        <div class="title">Informations compte:</div>
+                        <div class="field">
+                            <div class="label">Nom d'utilisateur<span style="color: red;">*</span></div>
+                            <input type="text">
+                        </div>
+                        <div class="field">
+                            <div class="label">Mot de passe<span style="color: red;">*</span></div>
+                            <input type="password">
+                        </div>
+                        <div class="field">
+                            <div class="label">Avatar</div>
+                            <input type="file">
+                        </div>
+                        <div class="field btns">
+                            <button class="prev-2 prev">Précédent</button>
+                            <button class="next-2 next">Suivant</button>
+                        </div>
+                        <h1 style="font-size: 15px;color:red">* Champs obligatoires</h1>
+                    </div>
+                    <div class="page">
+                        <div class="title">Aperçu:</div>
+                        <div class="profileHead">
+                                <div class="profile">
+                                    <div class="banner">
+                                        <div class="change-banner">
+                                        </div>
+                                    </div>
+                                    <div class="avatar__wrapper">
+                                        <div class="avatar">
+                                            <div class="change-avatar">
+                                            </div>
+                                            <div class="status-icon">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="headerTop">
+                                        <div class="headerText">
+                                            <p style="text-align: left;"" >
+                                                <strong>
+                                                    <span style="color: #ffffff;text-align: left;"><b>User0242ac120002</b></span>
+                                                </strong>
+                                                <span style="color: #ffffff;">
+                                                    <span style="color: #b6b8bb;text-align: left;"">#3648</span>
+                                                </span>
+                                            </p>
+                                            <div class="headerTag">
+                            
+                                                <p style="color:#b6b8bb;text-align: left;"" >Utilisateur de BlaBlaChat</p>
+                            
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                        </div>
+                        <div class="field btns">
+                            <button class="prev-3 prev">Précédent</button>
+                            <button class="submit">Terminer</button>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        </div>
     </div>
-</div>
+    <script src="script.js"></script>
+
 </body>
+
 </html>
---
+++ src/sign-up/script.js
@@ -0,0 +1,107 @@
+const slidePage = document.querySelector(".slide-page");
+const nextBtnFirst = document.querySelector(".firstNext");
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
+const prevBtnFourth = document.querySelector(".prev-3");
+const submitBtn = document.querySelector(".submit");
+const progressText = document.querySelectorAll(".step p");
+const progressCheck = document.querySelectorAll(".step .check");
+const bullet = document.querySelectorAll(".step .bullet");
+let current = 1;
+
+nextBtnFirst.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnSec.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnThird.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-75%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+submitBtn.addEventListener("click", function(){
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+  setTimeout(function(){
+    alert("Your Form Successfully Signed up");
+    location.reload();
+  },800);
+});
+
+prevBtnSec.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "0%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnThird.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnFourth.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+
+
+
+function setup(){
+  for (let i = 0; i < numParticles; i++){
+      particles.push(new Particle());
+  }
+}
+
+window.addEventListener('mousemove', (e) => {
+mouse.x = e.x;
+})
+
+function animate(){
+requestAnimationFrame(animate);
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+particles.forEach((particle, index) => {
+  particle.update();
+  if(particle.y + particle.radius < 0){
+    setTimeout(() => {
+      particles.splice(index, 1);
+    },0)
+    if(!user.login){
+     particles.push(new Particle()); 
+    }
+  }
+})
+}
+setup();
+animate();
+
+window.addEventListener('resize', function(){
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+})
