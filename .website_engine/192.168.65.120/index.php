68d42909bae4938f8c878c803d50889eb340a99c
HugoTby <hugotabarylapro@gmail.com>
2023-04-23 17:19:24 +0200

    ⬆️ Update website version to v2.0.0-beta.204

--- .website_engine/192.168.65.120/index.php
+++ .website_engine/192.168.65.120/index.php
@@ -3,14 +3,14 @@
 $ip = $_SERVER['REMOTE_ADDR'];
 
 // Vérifier si l'adresse IP de l'utilisateur est différente de 192.168.65.31
-if ($ip !== '192.168.65.31') {
+if ($ip !== '192.168.65.3') {
     // Rediriger l'utilisateur vers une page d'erreur ou afficher un message
     /*header('Location: /errors/maintenance.html');
     exit;*/
 
 ?>
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr" oncontextmenu="return false;">
 <head>
   <title>BlaBlaChat - Maintenance</title>
   <meta charset="utf-8">
@@ -19,6 +19,7 @@ if ($ip !== '192.168.65.31') {
 </head>
 <style>	body {
 			background-color: #f5f5f5;font-family: 'Montserrat', sans-serif;
+user-select: none;
 		}
 		.d-flex {
 			display:flex;
@@ -29,6 +30,8 @@ if ($ip !== '192.168.65.31') {
 		.container {
 			align-items: center;
 			max-width: 90%;
+
+			
 		}
 		.col-L {
 			padding:2em;
@@ -147,6 +150,7 @@ if ($ip !== '192.168.65.31') {
 		<svg class="logo" x="0px" y="0px" viewBox="0 0 90 90">
 		<g> <path class="color-white" d="M46.4,42.2c0.7,0,1.4-0.2,2-0.6l12.9-8.7l3.5-2.4L70,27l4.1-2.7c1.4-0.9,1.4-3,0-3.9L48.4,2.9 c-0.6-0.4-1.3-0.6-2-0.6s-1.4,0.2-2,0.6l-0.5,0.4L17.4,21.1c-1,0.7-1.6,1.8-1.6,3.1v3.5v4.9v10.3v10.3v13.5c0,1.2,0.6,2.4,1.6,3.1 l5.2,3.5c1.5,1,3.4-0.1,3.4-1.9V60.1V49.8v-3.4v-6.9V36c0-0.5,0.1-0.9,0.2-1.3c0.8-2.1,3.4-3.2,5.4-1.8l12.7,8.6 C44.9,42,45.6,42.2,46.4,42.2z M52.6,21.3l6.4-4.4c0.3-0.2,0.6-0.2,0.8-0.2s0.3,0.1,0.4,0.2l6.4,4.4c0.7,0.5,0.7,1.5,0,1.9 l-0.2,0.1l-6.3,4.2c-0.4,0.3-0.9,0.3-1.3,0l-6.4-4.4C51.9,22.7,51.9,21.7,52.6,21.3z"/> <path class="color-white" d="M74.2,60.3l-9.7,6.5L48.4,77.7c-0.6,0.4-1.3,0.6-2,0.6s-1.4-0.2-2-0.6l-4.9-3.3c-1.4-0.9-3.3,0.1-3.3,1.8v4.4 c0,1.2,0.6,2.4,1.6,3.1l6.6,4.4c0.6,0.4,1.3,0.6,2,0.6s1.4-0.2,2-0.6l21.9-14.8l5.7-3.8c0.6-0.4,1-1.1,1-1.9v-5.7 C76.9,60.3,75.3,59.5,74.2,60.3z"/> <path class="color-white" d="M73.1,35.2L48.5,51.8l-0.2,0.1c-0.6,0.4-1.3,0.6-2,0.6s-1.4-0.2-2-0.6l-3.8-2.6c-1.9-1.3-4.4,0.1-4.4,2.4v5 v3.2c0,1.2,0.6,2.4,1.6,3.1l6.6,4.4c0.6,0.4,1.3,0.6,2,0.6s1.4-0.2,2-0.6l9.3-6.3l0.7-0.5l6.4-4.3l4.5-3.1l6-4.1 c1-0.7,1.6-1.8,1.6-3.1v-3.2v-5.7C76.9,35.2,74.8,34.1,73.1,35.2z"/> </g>
 		</svg>
+<img style='height:70px;padding-left:80px' src='https://file.diplomeo-static.com/file/00/00/01/46/14675.svg'>
         <h1>maintenance</h1>
         <h2>Nous effectuons actuellement une maintenance planifiée.</h2>
         <hr>
@@ -186,6 +190,35 @@ if ($ip !== '192.168.65.31') {
     </div>
   </div>
 </body>
+<script>
+
+        // N'oubliez pas d'inclure le code présent à la ligne 14, dans votre balise html <body>
+
+        document.onkeydown=function(e)
+        {
+            if(event.keyCode == 123)
+            {
+                return false;
+            }
+            if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0))
+            {
+                return false;
+            }
+            if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0))
+            {
+                return false;
+            }
+            if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0))
+            {
+                return false;
+            }
+            if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0))
+            {
+                return false;
+            }
+    
+        }
+    </script>   
 </html>
 <?php
 } else {
